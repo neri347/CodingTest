1ff731c5606bb62a88ec6e7cc271c39bcfa02511
김민정 <rlaalswjd347@naver.com>
2025-01-07 21:50:13 +0900

    백준 기초 DP문제. 쉬운 계단수

---
+++ CodingTestExamples/EasyStairNumber.cpp
@@ -0,0 +1,47 @@
+/// 쉬운 계단 수
+/// https://www.acmicpc.net/problem/10844
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	long long mod = 1000000000;
+	vector<vector<int>> dp(n + 1, vector<int>(10, 0));
+
+	for (int i = 1; i < 10; ++i)
+	{
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= n; ++i)
+	{
+		for (int j = 0; j <= 9; ++j)
+		{
+			if (j == 0)
+			{
+				dp[i][j] = dp[i - 1][1] % mod;
+			}
+			else if (j == 9)
+			{
+				dp[i][j] = dp[i - 1][8] % mod;
+			}
+			else
+			{
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
+			}
+		}
+	}
+
+	long long sum = 0;
+	for (int i = 0; i <= 9; ++i)
+	{
+		sum += dp[n][i];
+	}
+	cout << sum % mod;
+
+	return 0;
+}
