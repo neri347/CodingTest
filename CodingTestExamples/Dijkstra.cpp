170b6103fc1a31d5e3f81fdf46348cd044469f6f
김민정 <rlaalswjd347@naver.com>
2025-01-29 21:38:54 +0900

    코테책 다익스트라 알고리즘 연습문제 다시 풀기

--- CodingTestExamples/Dijkstra.cpp
+++ CodingTestExamples/Dijkstra.cpp
@@ -5,6 +5,57 @@
 #include <queue>
 using namespace std;
 
+class Compare
+{
+public:
+	bool operator()(const pair<int, int>& a, const pair<int, int>& b)
+	{
+		return a.second > b.second;
+	}
+};
+
+// 시작 노드부터 각 노드까지의 최소 비용을 담은 벡터를 반환
+vector<int> solution(int start, int numNodes, vector<tuple<int, int, int>> edges)
+{
+	// 그래프 생성
+	vector<vector<pair<int, int>>> graph(numNodes);
+	for (const auto& [from, to, weight] : edges)
+	{
+		graph[from].push_back({ to, weight });
+	}
+
+	vector<int> distances(numNodes, INT_MAX);
+	vector<bool> isVisited(numNodes, false);
+	priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
+	distances[start] = 0;
+	pq.push({ start, 0 });
+
+	while (!pq.empty())
+	{
+		int vertex = pq.top().first;
+		int currentDistance = pq.top().second;
+		pq.pop();
+
+		if (isVisited[vertex])
+		{
+			continue;
+		}
+		isVisited[vertex] = true;
+
+		for (auto& [to, weight] : graph[vertex])
+		{
+			int newWeight = distances[vertex] + weight;
+			if (distances[to] > newWeight)
+			{
+				distances[to] = newWeight;
+				pq.push({ to, newWeight });
+			}
+		}
+	}
+
+	return distances;
+}
+
 int main()
 {
 	int start = 0;
