4520f0dbde2f6ff067b58d583ae7e70dd862f962
neri347 <rlaalswjd347@naver.com>
2025-02-13 23:23:55 +0900

    프로그래머스 코드챌린지 트리구성. 링크 추가 풀이 필요

--- CodingTestExamples/FindCodes.cpp
+++ CodingTestExamples/FindCodes.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_set>
+#include <set>
 #include <string>
 using namespace std;
 
@@ -12,54 +13,96 @@ using namespace std;
 /// ans[i] 는 i-1번째 시도로 넣어본 코드 중 맞는 숫자의 개수
 /// q.size() = m 이다
 
-int counts[31];
-unordered_set<string> codes;
+vector<vector<int>> qVec;
+vector<vector<bool>> isVisited;
+vector<int> ansVec;
+unordered_set<string> s;
 
-void BackTrack(int tryCount, int index, int chooseCount, const vector<vector<int>>& q, const vector<int>& ans)
+void DFS(int tryCount, int pickCount, int curIndex, set<int> nomination, vector<int> code)
 {
-	if (tryCount == q.size() + 1)
+	if (tryCount == qVec.size())
 	{
-		string temp = "";
-		for (int i = 0; i < 31; i++)
+		if (nomination.size() >= 5)
 		{
-			if (counts[i] > 0)
+			sort(code.begin(), code.end());
+			code.erase(unique(code.begin(), code.end()), code.end());
+			for (int i = 0; i < code.size(); i++)
 			{
-				temp += to_string(i);
+				if (nomination.find(code[i]) == nomination.end())
+				{
+					return;
+				}
 			}
-		}
-		if (temp.size() == 5)
-		{
-			codes.insert(temp);
+
+			string str = "";
+			for (auto& e : nomination)
+			{
+				str += to_string(e);
+			}
+			s.insert(str);
 		}
 		return;
 	}
 
-	for (int i = index; i < q[tryCount - 1].size(); i++)
+	// ansVec[tryCount]만큼 선택
+	// 선택되지 않은 숫자는 후보에서 제외
+
+	if (pickCount == ansVec[tryCount])
 	{
-		counts[q[tryCount - 1][i]]++;
-		if (chooseCount + 1 == ans[tryCount - 1])
+		for (int i = 0; i < qVec[tryCount].size(); i++)
 		{
-			BackTrack(tryCount + 1, 0, 0, q, ans);
+			if (!isVisited[tryCount][i])
+			{
+				nomination.erase(qVec[tryCount][i]);
+			}
 		}
-		else
-		{
-			BackTrack(tryCount, i + 1, chooseCount + 1, q, ans);
-		}
-		counts[q[tryCount - 1][i]]--;
+		DFS(tryCount + 1, 0, 0, nomination, code);
+		return;
+	}
+
+	for (int i = curIndex; i < qVec[tryCount].size(); i++)
+	{
+		int num = qVec[tryCount][i];
+		code.push_back(num);
+		isVisited[tryCount][i] = true;
+		DFS(tryCount, pickCount + 1, i + 1, nomination, code);
+		isVisited[tryCount][i] = false;
+		code.pop_back();
 	}
 }
 
-int solution(int n, vector<vector<int>> q, vector<int> ans)
-{
-	BackTrack(1, 0, 0, q, ans);	
-	return codes.size();
+int solution(int n, vector<vector<int>> q, vector<int> ans) {
+	qVec = q;
+	ansVec = ans;
+	isVisited.assign(q.size(), vector<bool>(q[0].size(), false));
+	set<int> nomination;
+	for (int i = 1; i <= n; i++)
+	{
+		nomination.insert(i);
+	}
+
+	DFS(0, 0, 0, nomination, {});
+	for (auto& str : s)
+	{
+		cout << str << endl;
+	}
+	return s.size();
 }
 
 int main()
 {
-	int n = 10;
+	/*int n = 10;
 	vector<vector<int>> q = { {1,2,3,4,5}, {3,6,7,8,9}, {1,6,7,8,9}, {1,3,5,7,9}, {2,4,6,8,10} };
-	vector<int> ans = { 2,3,4,3,1 };
+	vector<int> ans = { 2,3,4,3,1 };*/
+	/*int n = 10;
+	vector<vector<int>> q = { {1, 2, 3, 4, 5} ,{6, 7, 8, 9, 10},{3, 7, 8, 9, 10},{2, 5, 7, 9, 10},{3, 4, 5, 6, 7} };
+	vector<int> ans = { 2,3,4,3,3 };*/
+	/*int n = 15;
+	vector<vector<int>> q = { {2, 3, 9, 12, 13} ,{1, 4, 6, 7, 9},{1, 2, 8, 10, 12},{6, 7, 11, 13, 15},{1, 4, 10, 11, 14} };
+	vector<int> ans = { 2,1,3,0,1 };*/
+	int n = 10;
+	vector<vector<int>> q = { { 1,2,3,4,5 } };
+	vector<int> ans = { 5 };
 	cout << solution(n, q, ans);
 	return 0;
 }
