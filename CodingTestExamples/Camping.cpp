972f7359cbea81fd629acec30e4d62fa308192ec
neri347 <rlaalswjd347@naver.com>
2025-04-10 16:17:09 +0900

    프로그래머스 문제. 캠핑

---
+++ CodingTestExamples/Camping.cpp
@@ -0,0 +1,13 @@
+/// д╥гн
+/// https://school.programmers.co.kr/learn/courses/30/lessons/1833
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	vector<vector<int>> vec = { {0, 0}, {1, 1}, {0, 2}, {2, 0} };
+	sort(vec.begin(), vec.end());
+	return 0;
+}
